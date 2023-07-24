817b415b4777b665a13ac3934024b647d8a4c014
Thomas Howe <howethomas@users.noreply.github.com>
2023-07-23 20:39:17 -0700

    Final demo version.

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
-// debug-fetch-records.js
+// A node.js script that listens to a Matrix room and writes the messages to a vcon file.
+// It also posts the vcon file to a conserver on a regular basis.
+//
+// Original version for the IETF 117 Hackathon by Thomas McCarthy-Howe
+const dotenv = require('dotenv');
+dotenv.config();
+
+
 const fs = require("fs");
 const moment = require("moment");
 const sdk = require("matrix-js-sdk");
 const uuid = require("uuid");
-const myAccessToken = "xxxxxxx"
-const myUserId = "@howethomas:localhost";
-const vcon_path = "./vcons";
-const domain_name = "ietf.org"
-const url = "https://api.example.com/data";
+
+// CONFIGURATION
+const SYNAPSE_ACCESS_TOKEN = process.env.SYNAPSE_ACCESS_TOKEN || "xxxxxxx"
+const SYNAPSE_USER_ID = process.env.SYNAPSE_USER_ID || "@howethomas:localhost";
+const VCON_PATH = process.env.VCON_PATH || "./vcons";
+const DOMAIN_NAME = process.env.DOMAIN_NAME || "ietf.org"
+const CONSERVER_URL = process.env.CONSERVER_URL || "https://localhost:8000/vcon";
+const SYNAPSE_URL = process.env.SYNAPSE_URL || "http://localhost:8008";
+const VCON_UPLOAD_PERIOD_MS = process.env.VCON_UPLOAD_PERIOD_MS || 3600000; // 1 hour
+const DEFAULT_ROLE = process.env.DEFAULT_ROLE || "agent";
 
 const client = sdk.createClient({ 
-  baseUrl: "http://localhost:8008",
-  accessToken: myAccessToken,
-  userId: myUserId
+  baseUrl: SYNAPSE_URL,
+  accessToken: SYNAPSE_ACCESS_TOKEN,
+  userId: SYNAPSE_USER_ID
 });
 
 client.on("Room.timeline", function (event, room, toStartOfTimeline) {
+   // only use messages from the room
+   // and only if they are text messages
   if (event.getType() !== "m.room.message") {
-      return; // only use messages
+      return;
   }
   
   // Try to read the existing vcon file if it exists, otherwise create a new one
-  filename = `${vcon_path}/${room.name}:${event.event.room_id}.vcon`;
+  // The filename is based on the room name and the room id, so we can track
+  // the vcons even if the name changes.
+  filename = `${VCON_PATH}/${room.name}:${event.event.room_id}.vcon`;
+
+  // Make the vCon, based on the IETF vCon JSON schema
   let vcon = {};
   try {
+    // If the file exists, read it,
+    // otherwise, create a new vcon object.
+    // If the file is not valid JSON, we'll get an error
     vcon = JSON.parse(fs.readFileSync(filename, 'utf8'));
   } catch (err) {
+    // Create a new vcon object, based on the IETF vCon JSON schema
+    console.debug("Creating new vcon file: ", filename);
     vcon = {
       "uuid": event.event.room_id,
       "vcon": "0.0.1",
@@ -39,8 +62,10 @@ client.on("Room.timeline", function (event, room, toStartOfTimeline) {
 
     // If this is a new vcon, set the created_at timestamp in ISO format
     vcon.created_at = moment().toISOString();
+
     // Add the uuid, based on the domain name
-    vcon.uuid = uuid.v5(domain_name, uuid.v5.DNS);
+    vcon.uuid = uuid.v5(DOMAIN_NAME, uuid.v5.DNS);
+
     // Add the subject, based on the room name
     vcon.subject = "Recording of " + room.name;
   }
@@ -56,7 +81,7 @@ client.on("Room.timeline", function (event, room, toStartOfTimeline) {
     sender_party = {
       "tel": sender_name,
       "meta": {
-        "role": "agent",
+        "role": DEFAULT_ROLE,
         "extension": sender_id
       },
       "name": sender_name,
@@ -73,6 +98,9 @@ client.on("Room.timeline", function (event, room, toStartOfTimeline) {
   }
 
   // Now, add this message to the dialog
+  // This is the chatty part of the vCon. We make a new dialog object for every message.
+  // Perhaps a time where we could summarize this in an analysis object.
+  // However, we do want to keep the original message, so we can't just summarize it.
   dialog = {
     "body": event.event.content.body,
     "meta": {
@@ -98,9 +126,9 @@ console.log("Started client.")
 // Every hour, we'll check to see if we have any vcon files that are older than 24 hours.
 // If so, we'll POST them to the vcon server.
 setInterval(function() {
-  fs.readdirSync(vcon_path).forEach(file => {
+  fs.readdirSync(VCON_PATH).forEach(file => {
     console.log("Checking file: ", file)
-    let filename = `${vcon_path}/${file}`;
+    let filename = `${VCON_PATH}/${file}`;
     let vcon = JSON.parse(fs.readFileSync(filename, 'utf8'));
     let created_at = moment(vcon.created_at);
     let now = moment();
@@ -111,7 +139,7 @@ setInterval(function() {
       const jsonData = fs.readFileSync(filename);
 
       // POST the JSON data to the URL
-      const response = fetch(url, {
+      const response = fetch(CONSERVER_URL, {
         method: "POST",
         body: jsonData,
       });
@@ -127,5 +155,4 @@ setInterval(function() {
       fs.unlinkSync(filename);
     }
   });
-}, 3600000); // 1 hour
-
+}, VCON_UPLOAD_PERIOD_MS);
